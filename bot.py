b4984a39408290ea2ed498d6d8ced522540fe127
Kaleigh  Crawford <kaleighcrawford@Kaleighs-MacBook-Air.local>
2022-06-13 20:56:25 -0300

    created first request to mediawiki api

--- bot.py
+++ bot.py
@@ -1,23 +1,22 @@
-# bot.py
 import os
 
+import requests
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv('TOKEN')
 
+#retrieves an API response from the mediawiki endpoint
+response_API = requests.get('https://stardewvalleywiki.com/mediawiki/api.php?action=query&prop=revisions&titles=Blacksmith&rvslots=*&rvprop=content&formatversion=2')
+print(response_API.content)
+
+#creates a client at the discord endpoint.
 client = discord.Client()
 
-#When the client connects to the discord api, get the
-#current guild name and return it's data with a message.
+#waits for client to run 
 @client.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
